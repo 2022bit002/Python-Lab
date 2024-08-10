ee1bc510e5b3573c092948090af8264e6f82f3ad
2022bit002 <2022bit002@sggs.ac.in>
2024-08-10 12:29:13 +0530

    change it the pattern program validation of number add 10/08

--- pattern.py
+++ pattern.py
@@ -2,49 +2,57 @@
 
 def print_pattern(x):
 	
-	a=x
+	a=int(x)
+	w= x%a
+	
+	if(x<1 or  w!=0):
+		return "Invalid input !!! \nPlease give proper input" 
+	else:
+		x = int(x)
 	
 	# The upper part of the pattern:
-	for i in range(x+1):
-		for j in range(x+1-i): 
-			print(" ",end=' ')
-		print('*',end=' ')
-		
-		if(i>0):	
-			for k in range(2*i-1):
-				if(i==x):
-					if(k==x-1):
-						print(x ,end=' ')
+		for i in range(x+1):
+			for j in range(x+1-i): 
+				print(" ",end=' ')
+			print('*',end=' ')
+			
+			if(i>0):	
+				for k in range(2*i-1):
+					if(i==x):
+						if(k==x-1):
+							print(x ,end=' ')
+						else:
+							print(' ',end=' ')
 					else:
 						print(' ',end=' ')
-				else:
-					print(' ',end=' ')
-				
-			print('*',end =' ')
-		print()
+					
+				print('*',end =' ')
+			print()
+			
 		
-	
-	
-	# The lower pyramid of the pattern	
-	for q in range(x-1):
-		for w in range(q+2):
-			print(' ', end=' ')
-		print('*',end=' ')
-		for e in range(2*a-3):
-			print(' ',end=' ')
-		print('*',end=' ')
-		a=a-1
-		print()
-	
-	# The block at last of the pattern.
-	for r in range (x):
-		for t in range(2*x+3):
+		
+		# The lower pyramid of the pattern	
+		for q in range(x-1):
+			for w in range(q+2):
+				print(' ', end=' ')
 			print('*',end=' ')
-		print()
+			for e in range(2*a-3):
+				print(' ',end=' ')
+			print('*',end=' ')
+			a=a-1
+			print()
 		
-	
+		# The block at last of the pattern.
+		for r in range (x):
+			for t in range(2*x+3):
+				print('*',end=' ')
+			print()
+			
+		
+		return 'Pattern is printed'
 
 
-print_pattern(5) 
+print(print_pattern(4.0000))
+ 
 
 
