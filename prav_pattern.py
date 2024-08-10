85bd5dcd9fa986721bdf00590696f745f921a257
2022bit002 <2022bit002@sggs.ac.in>
2024-08-10 11:00:47 +0530

    commiting the practical pattern 11/08

--- prav_pattern.py
+++ prav_pattern.py
@@ -1,4 +1,46 @@
-def print_pattern(x):
+# first question of pattern 
+
+def print_patternQ1(x):
+	a=x
+	
+	# first the above triangle of the pattern
+	for i in range(x+1):
+		for j in range(x+1-i):
+			print(' ',end=" ")
+		print('+',end=' ')
+		if(i>0):
+			for k in range(2*i-1):
+				print(' ',end=' ')
+			print('+',end =' ')
+		
+				
+		print()
+	
+	
+	# the lower trinagle of the pattern
+	for q in range(x-1):
+		for w in range(q+2):
+			print(' ',end=' ')
+		print('+',end=' ')
+		for e in range(2*a+1-4):
+			print(' ',end=' ')
+		print('+',end=' ')
+		a=a-1
+		
+		print()
+		
+	# the last sign of the pattern
+	for r in range(1):
+		for t in range(x+1):
+			print(' ',end=' ')
+		print('-')
+	
+		
+# this triangle is similar to the one given above just have to replace the sign with another sign
+def print_patternQ2(x):
+	a=x
+	
+	# the above triangle of the pattern
 	for i in range(x+1):
 		for j in range(x+1-i):
 			print(' ',end=" ")
@@ -11,14 +53,24 @@ def print_pattern(x):
 				
 		print()
 		
+		
+	# the lower triangle of the pattern 
 	for q in range(x-1):
 		for w in range(q+2):
 			print(' ',end=' ')
-		print('+',end=' ')
+		print('-',end=' ')
+		for e in range(2*a+1-4):
+			print(' ',end=' ')
+		print('-',end=' ')
+		a=a-1
+		
 		print()
+	for r in range(1):
+		for t in range(x+1):
+			print(' ',end=' ')
+		print('-')
 	
 		
 		
 		
-		
-print_pattern(5)
+print_patternQ1(3)
