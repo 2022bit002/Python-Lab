917908d3d51d706d55e66c64026b0d8595c4a11e
2022bit002 <2022bit002@sggs.ac.in>
2024-08-14 22:17:10 +0530

    practical submission of the 08/08 on 14/08

--- prav_pattern.py
+++ prav_pattern.py
@@ -71,6 +71,67 @@ def print_patternQ2(x):
 		print('-')
 	
 		
+# The modulo function and its implementation
+
+def modulo_fun(numerator,denominator):
+
+	quoient = numerator // denominator 
+	remainder = numerator - (quoient * denominator)
+	return remainder	
 		
-		
-print_patternQ1(3)
+
+
+#  count function and itss implementation
+
+def count_overlaping(x,y,z):
+	
+	length = len(x)
+	count = -1
+	if(z==True):
+		for i in range(len(x)):
+			if(x[i] == y[0]):
+				j,m=i,0
+				for q in range(len(y)):	
+					if (j < length and x[j] == y[q] ):
+						m,j=m+1,j+1
+					else: 
+						break
+				if(m==len(y)):
+					if(count == -1):
+						count = 1	
+					else:
+						count +=1
+		return count;
+	
+	else:
+		i = 0
+		while (i <= length - len(y)):
+		    if (x[i] == y[0]):
+		        j = i
+		        m = 0
+		        for q in range(len(y)):
+		            if (j < length and x[j] == y[q]):  
+		                m += 1
+		                j += 1
+		            else:
+		                break  
+		        if m == len(y):  
+		            if count == -1:
+		                count = 1
+		            else:
+		                count += 1
+		            i = j  
+		        else:
+		            i += 1
+		    else:
+		        i += 1
+		return count
+
+
+
+
+
+
+
+#print(count_overlaping('pannnkajnnnmnnnnkaahafdhkkjjgh','nn',False))
+
