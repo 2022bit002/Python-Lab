26453d24b8b18380a38354b012ab7c98c1535bef
2022bit002 <2022bit002@sggs.ac.in>
2024-10-27 23:26:28 +0530

    commting the updated calinder module

--- calender.py
+++ calender.py
@@ -1,5 +1,36 @@
-def calender_print(dd,mm,yyyy):
-	day = ['sat','sun','mon','tues','wed','thurs','fri']
+	
+	
+	
+def print_cal(day,month,yy):
+	days_month = {1:31,2:29,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
+	week_day = ['sun', 'mon','tue','wed','thur','fri','sat']
+	first_day ={'sun':0,'mon':1,'tue':2,'wed':3,'thur':4,'fri':5,'sat':6}
+	gap_day ={'sun':'      '*0,'mon':'      '*1,'tue':'      '*2,'wed':'      '*3,'thur':'      '*4,'fri':'      '*5,'sat':'      '*6}
+	month_dic = {1:'jan',2:'feb',3:'mar',4:'april',5:'may',6:'june',7:'july',8:'august',9:'september',10:'october',11:'november',12:'december'}
+	print(yy)
+	print(month_dic[month])
+	for i in week_day:
+		print(i,end='   ')
+		
+	print()
+	k=(first_day['sat']-first_day[day]+1)%7
+	for i in range(1,days_month[month]+1):
+		if i==1:
+			print(end=gap_day[day])
+		if i<10: 
+			print(0,end='')
+		
+		print(i,end='    ')
+		if i%7==k:
+			print()
+			
+			
+	print()
+		
+
+	
+def calender_print(mm,yyyy,dd=1):
+	day = ['sat','sun','mon','tue','wed','thur','fri']
 	month = [0, 1, 4, 4, 0, 2, 5, 0, 3, 6, 1, 4, 6]
 	year ={16:6,17:4,18:2,19:0,20:6,21:4,22:2,23:0}
 	
@@ -20,8 +51,24 @@ def calender_print(dd,mm,yyyy):
 		final=final-1
 		
 	
-	return day[final]
+	print_cal(day[final],mm,yyyy)
+
+
+for i in range(1,13):
+	calender_print(i,2024)	
 	
 	
-print(calender_print(1,1,2004))
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
